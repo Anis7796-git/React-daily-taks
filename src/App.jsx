223dbd43f8e4edeb4f223f1c80ce0cfb659cfcfc
Anis7796-git <sk.anis6834@gmail.com>
2026-06-23 13:40:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import Center from './components/center/Center'
+import UseBootstrapIcon from './components/center/usebootstrap/UseBootstrapIcon'
+export default function App() {
+  return (
+    <>
+    <Header/>
+   <Center/>
+    <UseBootstrapIcon/>
+   <Footer/>
+    </>
+  )
+}
---
+++ src/components/center/usebootstrap/UseBootstrapIcon.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function UseBootstrapIcon() {
+  return (
+    <div><i className="bi-alarm"></i>
+   <i className="bi-airplane" style={{ fontSize: "2rem", color: "cornflowerblue" }}></i>
+
+</div>
+  )
+}
