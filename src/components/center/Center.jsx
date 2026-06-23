929063bf0c1800df4a82f232334e777f5d27c9f1
Anis7796-git <sk.anis6834@gmail.com>
2026-06-23 13:47:10 +0530

    Use BootStrap Icon

--- src/components/center/Center.jsx
+++ src/components/center/Center.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import UseBootstrapIcon from './usebootstrap/UseBootstrapIcon'
+
 
 export default function Center() {
   return (
     <>
-    
+     <UseBootstrapIcon/>
     </>
   )
 }
